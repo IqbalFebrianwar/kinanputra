c128833b5e5eef65f30dfda99fd10d7884143457
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-07-28 22:17:37 +0700

    Feat :Revisi

--- src/components/galery.tsx
+++ src/components/galery.tsx
@@ -7,6 +7,51 @@ const Galery = () => {
             <div className="grid lg:grid-cols-2 gap-10 py-10">
                 <div className="max-w-md">
                     <Image
+                        src="https://res.cloudinary.com/dehqnfmjw/image/upload/v1722179624/WhatsApp_Image_2024-07-28_at_22.11.25_252955d1_ef2okd.jpg"
+                        width={1000000}
+                        height={5000000}
+                        alt="image"
+                        className="object-cover h-48 w-96"
+                    />
+                </div>
+                <div className="max-w-md">
+                    <Image
+                        src="https://res.cloudinary.com/dehqnfmjw/image/upload/v1722179630/WhatsApp_Image_2024-07-28_at_22.12.25_ea0705ba_rvukuc.jpg"
+                        width={1000000}
+                        height={5000000}
+                        alt="image"
+                        className="object-cover h-48 w-96"
+                    />
+                </div>
+                <div className="max-w-md">
+                    <Image
+                        src="https://res.cloudinary.com/dehqnfmjw/image/upload/v1722179453/WhatsApp_Image_2024-07-28_at_21.59.27_5242ba12_j6stwg.jpg"
+                        width={1000000}
+                        height={5000000}
+                        alt="image"
+                        className="object-cover h-48 w-96"
+                    />
+                </div>
+                <div className="max-w-md">
+                    <Image
+                        src="https://res.cloudinary.com/dehqnfmjw/image/upload/v1722179457/WhatsApp_Image_2024-07-28_at_21.59.28_549c82a7_kenxy4.jpg"
+                        width={1000000}
+                        height={5000000}
+                        alt="image"
+                        className="object-cover h-48 w-96"
+                    />
+                </div>
+                <div className="max-w-md">
+                    <Image
+                        src="https://res.cloudinary.com/dehqnfmjw/image/upload/v1722179463/WhatsApp_Image_2024-07-28_at_21.59.28_a1bee48c_tqs7x8.jpg"
+                        width={1000000}
+                        height={5000000}
+                        alt="image"
+                        className="object-cover h-48 w-96"
+                    />
+                </div>
+                <div className="max-w-md">
+                    <Image
                         src="https://res.cloudinary.com/dehqnfmjw/image/upload/v1722155793/IMG-20240723-WA0018_wszhcq.jpg"
                         width={1000000}
                         height={5000000}
